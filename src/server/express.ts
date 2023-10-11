3b6a4d17f0f419dd79ab28deebfd39641a838fd7
k4itrun <103044629+k4itrun@users.noreply.github.com>
2023-10-10 19:39:59 -0500

    s

--- src/server/express.ts
+++ src/server/express.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import * as fs from "fs";
 
 const app: Express = express();
-const port: number = parseInt(process.env.PORT || '3000');
+const port: number = parseInt(process.env.PORT || 3000);
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ msg: '/tokens' });
